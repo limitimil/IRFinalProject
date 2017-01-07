4bd76bc10ef89ecd0f668ebe8f870ab14cba111f
limin <zlsh10340@gmail.com>
2017-01-07 23:03:43 +0800

    11:03 2017/1/7

---
+++ demo0107_2.py
@@ -0,0 +1,53 @@
+import sys
+import NHIRDSelector
+import NHIRDParser
+import NHIRDPieChart
+import matplotlib.pyplot as plt
+#define HV fmt
+CD101_fmt = [
+    ('ACODE_ICD9_1',132,146),
+    ('ACODE_ICD9_2',147,161),
+    ('ACODE_ICD9_3',162,176),
+    ('SEQ_NO', 52, 57),
+]
+#define my rule to filter data
+ICD9_include = (
+'174',
+'1740',
+'1741',
+'1742',
+'1743',
+'1744',
+'1745',
+'1746',
+'1748',
+'1749',
+'175',
+'1750',
+'1759',
+)
+
+
+if len(sys.argv)<2 :
+    sys.stderr.write('at least one input file need\n')
+    exit(7)
+
+npc = NHIRDPieChart.NHIRDPieChart(
+    NHIRDParser.NHIRDParser(HV_fmt)
+)
+npc.Filter = lambda x :\
+npc.parser.getByTag(x, 'ACODE_ICD9_1').strip()  in ICD9_include or
+npc.parser.getByTag(x, 'ACODE_ICD9_2').strip()  in ICD9_include or
+npc.parser.getByTag(x, 'ACODE_ICD9_3').strip()  in ICD9_include 
+
+npc.read(sys.argv[1])
+labels,sizes = npc.pieInformation('ACODE_ICD9_1')
+
+plt.pie(
+    sizes,
+    labels = labels,
+    autopct = '%1.1f%%',
+    startangle =140
+)
+plt.axis('equal')
+plt.show()
