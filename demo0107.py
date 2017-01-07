6f6db9fdba89ad7eb11a7ea48ca48bfa7ca513cb
limin <zlsh10340@gmail.com>
2017-01-07 21:28:37 +0800

    09:28 2017/1/7

--- demo0107.py
+++ demo0107.py
@@ -2,6 +2,7 @@ import sys
 import NHIRDSelector
 import NHIRDParser
 import NHIRDPieChart
+import matplotlib.pyplot as plt
 #define HV fmt
 HV_fmt = [
     ('ID',1,32),
@@ -35,4 +36,13 @@ npc = NHIRDPieChart.NHIRDPieChart(
 npc.Filter = lambda x : npc.parser.getByTag(x, 'ICD9CM_CODE').strip()  in\
 ICD9_include
 npc.read(sys.argv[1])
-print npc.pieInformation('ICD9CM_CODE')
+labels,sizes = npc.pieInformation('ICD9CM_CODE')
+
+plt.pie(
+    sizes,
+    labels = labels,
+    autopct = '%1.1f%%',
+    startangle =140
+)
+plt.axis('equal')
+plt.show()
